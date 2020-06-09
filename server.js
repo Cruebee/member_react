485a40b3ead83a2c29c685cd793e01a772fefed2
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-06-09 15:45:18 -0600

    create basic documentation page and additional test server features

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const http = require('http');
+  url = require('url');
+  fs = require('fs');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
+  var addr = request.url,
+    q = url.parse(addr, true),
+    filePath = "";
+
+    if (q.pathname.includes("documentation")) {
+      filePath = _dirname + "documentation.html";
+    }else{
+      filePath = "index.html";
+    }
+
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      function(err) {
+        if (err) {
+          console.log("Added to log!");
+        }
+      }
+    );
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Test server!\n');
 }).listen(8080);
 
 console.log('This Node test server is running on Port 8080.');
